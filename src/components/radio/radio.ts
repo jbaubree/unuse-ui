12c32c298636f36fe87cf1ebb4eccf7952c4b9fc
JB <jb.aubree@protonmail.com>
2023-07-21 10:37:32 +0200

    style: review radio group

--- src/components/radio/radio.ts
+++ src/components/radio/radio.ts
@@ -12,14 +12,14 @@ export const radioGroup = {
     wrapper: 'flex items-center justify-center bg-{color}-500 dark:bg-{color}-400',
     padding: 'p-1.5',
     rounded: 'rounded-full',
-    base: 'h-2.5 w-2.5 text-dark-500 dark:text-light-500 flex-shrink-0',
+    base: 'h-2.5 w-2.5 text-light-500 dark:text-dark-500 flex-shrink-0',
   },
   item: {
-    wrapper: 'w-full flex items-center gap-3',
+    wrapper: 'w-full flex items-center gap-3.75',
     contentWrapper: 'w-full flex items-center justify-between',
     base: 'flex flex-col items-start',
     icon: {
-      wrapper: 'flex items-center justify-center bg-{color}-500 dark:bg-{color}-400',
+      wrapper: 'flex items-center justify-center bg-{color}-200 dark:bg-{color}-800',
       padding: 'p-2',
       rounded: 'rounded-3px',
       base: 'h-4.5 w-4.5 text-dark-500 dark:text-light-500 flex-shrink-0',
