3bed8f9cecb8832d83dbe965a13305a598ab0706
JB <jb.aubree@protonmail.com>
2023-08-02 18:45:38 +0200

    style: remove transitions translate on tooltips and popover

--- src/components/popover/popover.ts
+++ src/components/popover/popover.ts
@@ -10,11 +10,11 @@ export const popover = {
   base: 'overflow-hidden focus:outline-none text-13px text-dark-500 dark:text-light-500',
   transition: {
     enterActiveClass: 'transition ease-out duration-200',
-    enterFromClass: 'opacity-0 translate-y-1',
-    enterToClass: 'opacity-100 translate-y-0',
+    enterFromClass: 'opacity-0',
+    enterToClass: 'opacity-100',
     leaveActiveClass: 'transition ease-in duration-150',
-    leaveFromClass: 'opacity-100 translate-y-0',
-    leaveToClass: 'opacity-0 translate-y-1',
+    leaveFromClass: 'opacity-100',
+    leaveToClass: 'opacity-0',
   },
   popper: {
     strategy: 'fixed',
--- src/components/tooltip/tooltip.ts
+++ src/components/tooltip/tooltip.ts
@@ -9,11 +9,11 @@ export const tooltip = {
   base: 'h-6 px-2 py-1.25 text-10px truncate text-light-500 dark:text-dark-500',
   transition: {
     enterActiveClass: 'transition ease-out duration-200',
-    enterFromClass: 'opacity-0 translate-y-1',
-    enterToClass: 'opacity-100 translate-y-0',
+    enterFromClass: 'opacity-0',
+    enterToClass: 'opacity-100',
     leaveActiveClass: 'transition ease-in duration-150',
-    leaveFromClass: 'opacity-100 translate-y-0',
-    leaveToClass: 'opacity-0 translate-y-1',
+    leaveFromClass: 'opacity-100',
+    leaveToClass: 'opacity-0',
   },
   popper: {
     strategy: 'fixed',
