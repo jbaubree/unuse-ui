09f671325ccbabcbd646ba002b6ccd3d1c54e92f
JB <jb.aubree@protonmail.com>
2023-08-16 14:20:14 +0200

    feat(avatar): add 2xl size

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -24,6 +24,7 @@ export const avatar = {
     'md': 'h-10 w-10 text-sm',
     'lg': 'h-12 w-12 text-base',
     'xl': 'h-14 w-14 text-lg',
+    '2xl': 'h-18.75 w-18.75 text-lg',
   } as Record<Size, string>,
   chip: {
     base: 'absolute block rounded-full ring-1 ring-white dark:ring-dark-500',
