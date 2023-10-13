355521f46196efcf60da5239e9d048390ee699ea
JB <jb.aubree@protonmail.com>
2023-10-13 15:27:55 +0200

    style(popover): update shadow size

--- src/components/popover/popover.ts
+++ src/components/popover/popover.ts
@@ -6,7 +6,7 @@ export const popover = {
   rounded: 'rounded-5px',
   padding: '',
   ring: 'ring-1 ring-light-300 dark:ring-dark-500',
-  shadow: 'shadow',
+  shadow: 'shadow-lg',
   base: 'overflow-hidden focus:outline-none text-13px text-dark-500 dark:text-light-500',
   transition: {
     enterActiveClass: 'transition ease-out duration-200',
