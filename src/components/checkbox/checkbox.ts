a012c7442d3116613d7b3eed78a90636109689ba
JB <jb.aubree@protonmail.com>
2024-01-11 18:04:26 +0100

    style(checkbox): add gap

--- src/components/checkbox/checkbox.ts
+++ src/components/checkbox/checkbox.ts
@@ -2,6 +2,6 @@ import { radio } from '~/components/radio/radio'
 
 export const checkbox = {
   ...radio,
-  wrapper: 'relative flex flex-col items-start',
+  wrapper: 'relative flex flex-col items-start gap-3',
   base: `${radio.base} rounded`,
 }
